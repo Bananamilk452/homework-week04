b5bd1d6ebc6f4972587b31e0aa8c72a30b73176d
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 19:38:44 +0900

    Refactor: Remove unused import 'package:flutter/services.dart'

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
