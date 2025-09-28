3b7eecb2fdd76d482c4814820a427550d1231c5f
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 19:53:44 +0900

    feat: Define text themes for YouAndI app
    This commit defines text themes (headlineLarge, headlineMedium, bodyLarge, bodyMedium) for the YouAndI app, setting color, font size, weight, and family.

--- lib/week04/you_and_i/main.dart
+++ lib/week04/you_and_i/main.dart
@@ -4,10 +4,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'sunflower',
-        textTheme: TextTheme(
-          
+      theme: ThemeData( // 1 테마를 지정할 수 있는 클래스
+        fontFamily: 'sunflower', // 기본 글씨체
+        textTheme: TextTheme(  // 2 글씨 테마를 적용할 수 있는 클래스
+          headlineLarge: TextStyle( // headline1 스타일 정의
+            color: Colors.white, // 글 색상
+            fontSize: 80.0, // 크기
+            fontWeight: FontWeight.w700, // 글 투께
+            fontFamily: 'parisienne', // 글씨체
+          ),
+          headlineMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0
+          )
         )
       ),
       home: HomeScreen()
