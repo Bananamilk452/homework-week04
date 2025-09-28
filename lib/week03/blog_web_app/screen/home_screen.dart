3745302f6fa31271e43edca4c54e7dbfac81af6a
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 19:25:28 +0900

    feat: Implement basic HomeScreen for blog web app
    This commit introduces the basic structure for the HomeScreen, including a Scaffold with a simple 'Home Screen' text. It also includes the main.dart to run the HomeScreen.

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -1,1 +1,15 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+
+  // 1 const 생성자
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
+}
