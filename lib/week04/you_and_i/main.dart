6e62cbe06b3b7dc563c9877fd30efdb99fa4ccf4
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 19:50:44 +0900

    feat(you_and_i): Apply sunflower font theme

--- lib/week04/you_and_i/main.dart
+++ lib/week04/you_and_i/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          
+        )
+      ),
       home: HomeScreen()
     )
   );
