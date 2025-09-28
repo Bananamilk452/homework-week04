c25596aee5089acf23c46000a7b9dafb81b4e763
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 20:06:14 +0900

    feat(home_screen): Implement date picker dialog with state update
    This commit implements a date picker dialog using CupertinoDatePicker, allowing the user to select a date. The selected date is then used to update the  state. The dialog is also styled with a white background and fixed height.

--- lib/week04/you_and_i/screen/home_screen.dart
+++ lib/week04/you_and_i/screen/home_screen.dart
@@ -42,14 +42,24 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context, // 3 보여줄 다이얼로그 빌드
       builder: (BuildContext context) {
         // 4 날짜 선택하는 다이얼로그
-        return CupertinoDatePicker(
-          // 5 시간 제외하고 날짜만 선택하기
-          mode: CupertinoDatePickerMode.date,
-          onDateTimeChanged: (DateTime date) {
-
-          },
+        return Align( // 1 정렬을 지정하는 위젯
+          alignment: Alignment.bottomCenter, // 2 아래 중간으로 정렬
+          child: Container(
+            color: Colors.white, // 배경색 흰색 지정
+            height: 300, // 높이 300 지정
+            child: CupertinoDatePicker(
+              // 5 시간 제외하고 날짜만 선택하기
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  firstDay = date;
+                });
+              },
+            )
+          ),
         );
-      }
+      },
+      barrierDismissible: true
     );
   }
 }
