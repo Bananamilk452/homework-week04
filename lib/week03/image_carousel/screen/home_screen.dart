2c0299df6daa4fcb51519648c81d2c68d61ad643
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 19:35:58 +0900

    feat: Implement basic image carousel using PageView
    This commit introduces a basic image carousel using the PageView widget in the HomeScreen. It displays a series of images from local assets.

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const Home
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView( // 1 PageView 추가
+        children: [1, 2, 3, 4, 5] // 2 샘플 리스트 생성
+            .map( // 3 위젯으로 매핑
+        (number) => Image.asset('asset/img/image_$number.jpeg')
+            )
+            .toList(),
+      )
+    );
+  }
 }
