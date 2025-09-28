5b68a2f5face084514d3992a770e7204219365bc
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-28 19:36:28 +0900

    Fix: Image carousel aspect ratio
    The images in the image carousel now properly fill the screen.

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -9,7 +9,10 @@ class HomeScreen extends StatelessWidget {
       body: PageView( // 1 PageView 추가
         children: [1, 2, 3, 4, 5] // 2 샘플 리스트 생성
             .map( // 3 위젯으로 매핑
-        (number) => Image.asset('asset/img/image_$number.jpeg')
+        (number) => Image.asset(
+                'asset/img/image_$number.jpeg',
+                fit: BoxFit.cover
+              )
             )
             .toList(),
       )
